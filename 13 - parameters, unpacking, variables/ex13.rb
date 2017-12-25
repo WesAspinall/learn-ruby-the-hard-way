7398647c45db4dd8b54c20c8808da13a4821c6b1
wesaspinall <wes@wesaspinall.com>
2017-12-24 23:28:44 -0500

    argv

---
+++ 13 - parameters, unpacking, variables/ex13.rb
@@ -0,0 +1,6 @@
+
+first, second, third, fourth = ARGV #argument variable, holds what you pass into ruby script
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
